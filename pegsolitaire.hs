4198c22235f9a8a510c9e0e27ef20bc9b51cb508
frehlid <frehlid@student.ubc.ca>
2023-02-24 15:57:37 -0800

    Implemented play and basic solver

--- pegsolitaire.hs
+++ pegsolitaire.hs
@@ -2,7 +2,8 @@ import Data.Array
 import Data.Maybe
 import Data.List
 import Data.Maybe (isJust)
-import Control.Monad.RWS (MonadState(state))
+import TreeDict
+
 type Board = [[BoardEntry]]
 type BoardEntry = (Coordinate, Int) -- status is one of 1=filled 0=empty -1=invalid
 type Coordinate = String
@@ -17,6 +18,9 @@ data Result = EndOfGame State
     deriving (Eq, Show)
 
 type Game = Move -> State -> Result
+
+
+type Mem = Dict State (Board, [Move])
 
 
 -------- USAGE --------
@@ -46,6 +50,34 @@ starting_board =
   [("",-1),("",-1),("E1",1),("E2",1),("E3",1),("E4",1),("E5",1),("E6",1),("E7",1),("",-1),("",-1)],
   [("",-1),("",-1),("",-1),("",-1),("F3",1),("F4",1),("F5",1),("",-1),("",-1),("",-1),("",-1)],
   [("",-1),("",-1),("",-1),("",-1),("G3",1),("G4",1),("G5",1),("",-1),("",-1),("",-1),("",-1)],
+  [("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1)],
+  [("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1)]]
+
+almost_win_board :: Board
+almost_win_board =
+  [[("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1)],
+  [("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1)],
+  [("",-1),("",-1),("",-1),("",-1),("A3",0),("A4",0),("A5",0),("",-1),("",-1),("",-1),("",-1)],
+  [("",-1),("",-1),("",-1),("",-1),("B3",0),("B4",0),("B5",0),("",-1),("",-1),("",-1),("",-1)],
+  [("",-1),("",-1),("C1",0),("C2",0),("C3",0),("C4",0),("C5",0),("C6",0),("C7",0),("",-1),("",-1)],
+  [("",-1),("",-1),("D1",0),("D2",1),("D3",1),("D4",0),("D5",0),("D6",0),("D7",0),("",-1),("",-1)],
+  [("",-1),("",-1),("E1",0),("E2",0),("E3",0),("E4",0),("E5",0),("E6",0),("E7",0),("",-1),("",-1)],
+  [("",-1),("",-1),("",-1),("",-1),("F3",0),("F4",0),("F5",0),("",-1),("",-1),("",-1),("",-1)],
+  [("",-1),("",-1),("",-1),("",-1),("G3",0),("G4",0),("G5",0),("",-1),("",-1),("",-1),("",-1)],
+  [("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1)],
+  [("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1)]]
+
+almost_lose_board :: Board
+almost_lose_board =
+  [[("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1)],
+  [("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1)],
+  [("",-1),("",-1),("",-1),("",-1),("A3",0),("A4",0),("A5",0),("",-1),("",-1),("",-1),("",-1)],
+  [("",-1),("",-1),("",-1),("",-1),("B3",0),("B4",0),("B5",0),("",-1),("",-1),("",-1),("",-1)],
+  [("",-1),("",-1),("C1",0),("C2",0),("C3",1),("C4",0),("C5",0),("C6",0),("C7",1),("",-1),("",-1)],
+  [("",-1),("",-1),("D1",0),("D2",0),("D3",1),("D4",0),("D5",0),("D6",0),("D7",0),("",-1),("",-1)],
+  [("",-1),("",-1),("E1",0),("E2",0),("E3",0),("E4",0),("E5",0),("E6",0),("E7",0),("",-1),("",-1)],
+  [("",-1),("",-1),("",-1),("",-1),("F3",0),("F4",0),("F5",0),("",-1),("",-1),("",-1),("",-1)],
+  [("",-1),("",-1),("",-1),("",-1),("G3",0),("G4",0),("G5",0),("",-1),("",-1),("",-1),("",-1)],
   [("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1)],
   [("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1)]]
 
@@ -107,16 +139,121 @@ get_board_entry board coord =
     in find (match_entry coord) (board!!(row+2))
 
 
---Nate
---display_board :: Board -> IO ()
---display_board board =
+playGame = play (State starting_board (generate_valid_moves starting_board))
+playEndGameWin = play (State almost_win_board (generate_valid_moves almost_win_board))
+playEndGameLose = play (State almost_lose_board (generate_valid_moves almost_lose_board))
+
+endGameStateWin = (State almost_win_board (generate_valid_moves almost_win_board))
+endGameStateLose = (State almost_lose_board (generate_valid_moves almost_lose_board))
+
+play :: State -> IO ()
+play (State bd moves) =
+  do
+    display_board bd
+    move <- parse_move moves
+    let newState = update_game_state (State bd moves) move
+    if (win newState) then do
+        display_board (get_board newState)
+        putStrLn "You win!"
+    else if (lose newState) then do
+        display_board (get_board newState)
+        putStrLn "You lose :("
+    else do
+        putStrLn "Please select one of the following options:"
+        putStrLn "Continue - Hint - Solve"
+        play newState
+
+
+solve :: State -> [Move] -> Mem -> Maybe ((State, [Move]), Mem)
+solve (State bd moves) moves_played memory =
+    let stored_result = getval bd memory in
+
+    if (isJust stored_result) then
+        Just (((State bd moves), fromJust stored_result), memory)
+    if (win (State bd moves)) then
+        Just (((State bd moves), moves_played), memory)
+    else if (lose (State bd moves)) then
+        Nothing
+    else
+        let states = map (\ move -> (update_game_state (State bd moves) move, (move:moves_played))) moves in
+        let results = map (\ state ->
+            let result = solve (fst (fst state)) (snd (fst state)) memory) states in
+            if (isJust result) then
+                let memory = insertval (fst (fst (fromJust result))) (snd (fst (fromJust result))) memory
+        foldr (\ result rest -> if isJust result then result else rest) Nothing results
+
+
+
+
+
+parse_move :: [Move] -> IO Move
+parse_move moves =
+    do
+        putStrLn (show moves)
+        putStrLn "Please enter the starting coordinate of the move:"
+        startCoord <- get_coordinate
+        putStrLn "Please enter the ending coordinate of the move:"
+        endCoord <- get_coordinate
+        let move = get_move startCoord endCoord moves
+        if isJust move then do
+            return (fromJust move)
+        else do
+            let line = (startCoord ++ " to " ++ endCoord ++ " is not a valid move. Please try again:")
+            putStrLn line
+            parse_move moves
+
+get_move :: String -> String -> [Move] -> Maybe Move
+get_move start end  moves =
+    let filtered_start = filter (\ (st,mi,en) -> (st == start)) moves
+        filtered_end = filter (\ (st, mi, en) -> (en == end)) filtered_start
+    in
+        if (length filtered_end) == 1 then
+            Just (head filtered_end)
+        else
+            Nothing
+
+
+get_coordinate ::  IO String
+get_coordinate =
+    do
+      coord <- getLine
+      if not (check_valid_coord coord) then do
+         let line = (coord ++ " is not a valid coordinate. Please try again:")
+         putStrLn line
+         coord <- get_coordinate
+         return coord
+      else return coord
+
+
+display_board :: Board -> IO()
+display_board board =
+  do
+     let borderless = [init (init (drop 2 row)) | row <- (init (init (drop 2 board)))]
+     putStrLn "   1 2 3 4 5 6 7"
+     putStrLn (foldl (\acc (l, row) -> acc ++ l ++ "  " ++ row ++ "\n")
+                     ""
+                     (zip [['A'],['B'],['C'],['D'],['E'],['F'],['G']]
+                          [(unwords [if y == -1
+                                      then " "
+                                        else if y == 1
+                                                then "."
+                                                  else "o"
+                                                  | (x,y) <- borderless!!row]) | row<-[0..6]]))
 
 
 --get_move :: IO () -> Move
 --get_move =
 
+get_board :: State -> Board
+get_board (State board _) = board
+
 check_valid_move :: State -> Move -> Bool
-check_valid_move st mv = True
+check_valid_move (State bd mvs) mv = elem mv mvs
+
+check_valid_coord :: String -> Bool
+check_valid_coord coord =
+    let flattenedBoard = concat starting_board in
+    length (filter (\ entry -> (fst entry) == coord) flattenedBoard) == 1
 
 -- Returns a new game state with the move performed
 update_game_state :: State -> Move -> State
@@ -159,8 +296,26 @@ match_entry :: Coordinate -> BoardEntry -> Bool
 match_entry coord entry = fst entry == coord
 
 -- Only one position left in the board with a filled peg
+--win :: State -> Bool
+--win (State board moves) =
+--  let flattenedBoard = concat board in
+--  length (filter (\entry -> (snd entry) == 1) flattenedBoard) == 1
+
 win :: State -> Bool
 win (State board moves) =
-  let flattenedBoard = concat board in 
-  length (filter (\entry -> (snd entry) == 1) flattenedBoard) == 1
+   let flattenedBoard = concat board in
+   foldr (\ entry acc ->
+        if (fst entry == "D4") then
+            acc && (snd entry == 1)
+        else
+            acc && (snd entry /= 1)
+   ) True flattenedBoard
 
+
+lose :: State -> Bool
+lose (State board moves) =
+    (length moves) == 0
+
+-- Parse user input into a move
+-- Validate that the move is allowed (legal and valid)
+-- Update the game state
