4199509f434145ca71297421e66452fbedfc3a93
frehlid <frehlid@student.ubc.ca>
2023-02-26 13:24:43 -0800

    Many DFS implementations, none really fast enough

--- pegsolitaire.hs
+++ pegsolitaire.hs
@@ -3,14 +3,12 @@ import Data.Maybe
 import Data.List
 import Data.Maybe (isJust)
 import TreeDict
+import qualified Data.Set as Set
 
 type Board = [[BoardEntry]]
 type BoardEntry = (Coordinate, Int) -- status is one of 1=filled 0=empty -1=invalid
 type Coordinate = String
 type Move = (Coordinate, Coordinate, Coordinate) -- (Start, Middle, End)
-
-
-
 
 -- Based off of magicsum.hs from class
 data State = State Board [Move]
@@ -23,7 +21,7 @@ data Result = EndOfGame State
 type Game = Move -> State -> Result
 
 
-type Mem = Dict State [Move]
+type Mem = Dict State Bool
 
 
 -------- USAGE --------
@@ -64,8 +62,8 @@ win_board =
   [("",-1),("",-1),("",-1),("",-1),("A3",0),("A4",0),("A5",0),("",-1),("",-1),("",-1),("",-1)],
   [("",-1),("",-1),("",-1),("",-1),("B3",0),("B4",0),("B5",0),("",-1),("",-1),("",-1),("",-1)],
   [("",-1),("",-1),("C1",0),("C2",0),("C3",0),("C4",0),("C5",0),("C6",0),("C7",0),("",-1),("",-1)],
-  [("",-1),("",-1),("D1",0),("D2",0),("D3",0),("D4",1),("D5",0),("D6",0),("D7",0),("",-1),("",-1)],
-  [("",-1),("",-1),("E1",0),("E2",0),("E3",0),("E4",0),("E5",0),("E6",0),("E7",0),("",-1),("",-1)],
+  [("",-1),("",-1),("D1",0),("D2",1),("D3",0),("D4",0),("D5",0),("D6",0),("D7",0),("",-1),("",-1)],
+  [("",-1),("",-1),("E1",0),("E2",1),("E3",1),("E4",0),("E5",0),("E6",0),("E7",0),("",-1),("",-1)],
   [("",-1),("",-1),("",-1),("",-1),("F3",0),("F4",0),("F5",0),("",-1),("",-1),("",-1),("",-1)],
   [("",-1),("",-1),("",-1),("",-1),("G3",0),("G4",0),("G5",0),("",-1),("",-1),("",-1),("",-1)],
   [("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1)],
@@ -78,13 +76,27 @@ almost_win_board =
   [("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1)],
   [("",-1),("",-1),("",-1),("",-1),("A3",0),("A4",0),("A5",0),("",-1),("",-1),("",-1),("",-1)],
   [("",-1),("",-1),("",-1),("",-1),("B3",0),("B4",0),("B5",0),("",-1),("",-1),("",-1),("",-1)],
-  [("",-1),("",-1),("C1",1),("C2",1),("C3",1),("C4",0),("C5",0),("C6",0),("C7",0),("",-1),("",-1)],
-  [("",-1),("",-1),("D1",1),("D2",1),("D3",1),("D4",0),("D5",0),("D6",0),("D7",0),("",-1),("",-1)],
-  [("",-1),("",-1),("E1",1),("E2",1),("E3",1),("E4",0),("E5",0),("E6",0),("E7",0),("",-1),("",-1)],
-  [("",-1),("",-1),("",-1),("",-1),("F3",1),("F4",1),("F5",0),("",-1),("",-1),("",-1),("",-1)],
+  [("",-1),("",-1),("C1",0),("C2",0),("C3",0),("C4",0),("C5",0),("C6",0),("C7",0),("",-1),("",-1)],
+  [("",-1),("",-1),("D1",0),("D2",1),("D3",1),("D4",0),("D5",0),("D6",0),("D7",0),("",-1),("",-1)],
+  [("",-1),("",-1),("E1",0),("E2",0),("E3",0),("E4",0),("E5",0),("E6",0),("E7",0),("",-1),("",-1)],
+  [("",-1),("",-1),("",-1),("",-1),("F3",0),("F4",0),("F5",0),("",-1),("",-1),("",-1),("",-1)],
   [("",-1),("",-1),("",-1),("",-1),("G3",0),("G4",0),("G5",0),("",-1),("",-1),("",-1),("",-1)],
   [("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1)],
   [("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1)]]
+
+waterloo_board :: Board
+waterloo_board =
+    [[("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1)],
+    [("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1)],
+    [("",-1),("",-1),("",-1),("",-1),("A3",0),("A4",0),("A5",0),("",-1),("",-1),("",-1),("",-1)],
+    [("",-1),("",-1),("",-1),("",-1),("B3",0),("B4",1),("B5",0),("",-1),("",-1),("",-1),("",-1)],
+    [("",-1),("",-1),("C1",0),("C2",0),("C3",1),("C4",0),("C5",1),("C6",0),("C7",0),("",-1),("",-1)],
+    [("",-1),("",-1),("D1",0),("D2",0),("D3",1),("D4",0),("D5",0),("D6",1),("D7",0),("",-1),("",-1)],
+    [("",-1),("",-1),("E1",0),("E2",0),("E3",0),("E4",0),("E5",0),("E6",0),("E7",0),("",-1),("",-1)],
+    [("",-1),("",-1),("",-1),("",-1),("F3",0),("F4",0),("F5",0),("",-1),("",-1),("",-1),("",-1)],
+    [("",-1),("",-1),("",-1),("",-1),("G3",0),("G4",0),("G5",0),("",-1),("",-1),("",-1),("",-1)],
+    [("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1)],
+    [("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1)]]
 
 almost_lose_board :: Board
 almost_lose_board =
@@ -93,12 +105,14 @@ almost_lose_board =
   [("",-1),("",-1),("",-1),("",-1),("A3",0),("A4",0),("A5",0),("",-1),("",-1),("",-1),("",-1)],
   [("",-1),("",-1),("",-1),("",-1),("B3",0),("B4",0),("B5",0),("",-1),("",-1),("",-1),("",-1)],
   [("",-1),("",-1),("C1",0),("C2",0),("C3",1),("C4",0),("C5",0),("C6",0),("C7",1),("",-1),("",-1)],
-  [("",-1),("",-1),("D1",0),("D2",0),("D3",1),("D4",0),("D5",0),("D6",0),("D7",0),("",-1),("",-1)],
+  [("",-1),("",-1),("D1",0),("D2",1),("D3",1),("D4",0),("D5",0),("D6",0),("D7",0),("",-1),("",-1)],
   [("",-1),("",-1),("E1",0),("E2",0),("E3",0),("E4",0),("E5",0),("E6",0),("E7",0),("",-1),("",-1)],
   [("",-1),("",-1),("",-1),("",-1),("F3",0),("F4",0),("F5",0),("",-1),("",-1),("",-1),("",-1)],
   [("",-1),("",-1),("",-1),("",-1),("G3",0),("G4",0),("G5",0),("",-1),("",-1),("",-1),("",-1)],
   [("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1)],
   [("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1),("",-1)]]
+
+
 
 --Nate generates legal moves
 legal_moves :: Int -> Int -> Board -> [Maybe Move]
@@ -158,7 +172,8 @@ get_board_entry board coord =
     in find (match_entry coord) (board!!(row+2))
 
 
-playGame = play (State starting_board (generate_valid_moves starting_board))
+startState = (State starting_board (generate_valid_moves starting_board))
+playGame = play startState
 playEndGameWin = play (State almost_win_board (generate_valid_moves almost_win_board))
 playEndGameLose = play (State almost_lose_board (generate_valid_moves almost_lose_board))
 
@@ -206,40 +221,188 @@ play (State bd moves) =
 --        newMemory =  insertval (get_board newState) newMoves in
 --        solve
 
-solve :: State -> [Move] -> [(State, Move)] -> [State] -> Maybe ((State, [Move]))
-solve (State bd moves) played next_states visited =
-    let nextStatesNew = (map (\ move -> ((update_game_state (State bd moves) move), move)) moves) ++ next_states
-        nextStatesFiltered = filter (\ (ns, _) -> notElem ns visited) nextStatesNew in
-    if (win (State bd moves)) then
-        Just ((State bd moves), played)
-    else if (nextStatesFiltered == []) then
-        Nothing
+--solve :: State -> [Move] -> [(State, Move)] -> [State] -> Maybe ((State, [Move]))
+--solve (State bd moves) played next_states visited =
+--    let nextStatesNew = (map (\ move -> ((update_game_state (State bd moves) move), move)) moves)
+--        nextStatesFiltered = (filter (\ (ns, _) -> notElem ns visited) nextStatesNew) ++ next_states in
+--    if (win (State bd moves)) then
+--        Just ((State bd moves), played)
+--    else if (nextStatesFiltered == []) then
+--        Nothing
+--    else
+--        let (nextStateTuple:restStates) = nextStatesFiltered
+--            nextState = fst nextStateTuple
+--            nextMove = snd nextStateTuple in
+--           case  solve nextState (played ++ [nextMove]) restStates ((State bd moves):visited) of
+--            Just solution -> Just solution
+--            Nothing ->  solve (State bd moves) played restStates ((State bd moves):visited)
+--
+--solve :: State -> [Move] -> [(State, Move, [Move])] -> [State] -> (Bool, [Move])
+--solve (State bd moves) played nextStates visited =
+--    let newStates = map (\ move -> ((update_game_state (State bd moves) move), move)) moves
+--        filteredStates = filter (\ (newState, _) -> notElem newState visited) newStates
+--        statesWithPlayed = map (\ (s,m) -> (s,m,played)) filteredStates
+--        nextStatesNew = statesWithPlayed ++ nextStates in
+--    if elem (State bd moves) visited then
+--            (False, played)
+--        else if (win (State bd moves)) then
+--             (True, played)
+--        else if (nextStatesNew == []) then
+--             (False, played)
+--    else
+--        let (nextStateTriple:restStates) = nextStatesNew
+--            (nextState, nextMove, nextPlayed) = nextStateTriple in
+--            solve nextState (nextPlayed ++ [nextMove]) restStates ((State bd moves):visited)
+
+
+stateInVisited state visited = getval state visited
+
+solve :: [(State, Move, [Move])] -> Mem -> (Bool, [Move], Mem)
+solve frontier visited =
+    if (frontier == []) then
+            (False, [], visited)
     else
-        let (nextStateTuple:restStates) = nextStatesFiltered
-            nextState = fst nextStateTuple
-            nextMove = snd nextStateTuple in
-            solve nextState (nextMove:played) restStates ((State bd moves):visited)
+        let (newState@(State board moves), newMove, newPlayed) = head frontier in
+        if (win newState) then
+            (True, newPlayed, visited)
+        else case stateInVisited newState visited of
+            Just bool -> solve (tail frontier) visited
+            Nothing ->
+                    let newFrontierValues = map (\ move -> ((update_game_state newState move), move, (move:newPlayed))) moves
+                        nextFrontier = newFrontierValues ++ frontier
+                        nextVisited = insertval newState True visited
+                        res = solve (tail nextFrontier) nextVisited in
+                        case res of
+                            (True, _, _) -> res
+                            (False, _, rvisited) -> solve (tail nextFrontier) rvisited
 
 
--- Hash must return the same value for rotationally symmetric boards
+test_frontier = map (\ move -> ((update_game_state startState move), move, (move:[]))) (generate_valid_moves (get_board startState))
 
--- LRTB
---hash board =
+----
+--solve :: State -> [Move] -> Set.Set State -> Maybe ((State, [Move]))
+--solve (State bd moves) played visited =
+--    let nextStatesNew = (map (\ move -> ((update_game_state (State bd moves) move), move)) moves)
+--        nextStatesFiltered = (filter (\ (ns, _) -> not (Set.member ns visited)) nextStatesNew) in
+--    if (win (State bd moves)) then
+--        Just ((State bd moves), played)
+--    else if (nextStatesFiltered == []) then
+--        Nothing
+--    else
+--        case solve_helper nextStatesFiltered played (Set.insert (State bd moves) visited) of
+--            Just solution -> solution
+--            Nothing -> Nothing
+--
+--
+---- Calls solve on each nextState
+----solve_helper :: [(State, Move)] -> [Move] -> Set.Set State -> Maybe (State, [Move])
+--solve_helper nextStates played visited =
+--    if (nextStates == []) then
+--        Nothing
+--    else
+--        let (nextState, nextMove) = head nextStates
+--            result = solve nextState (nextMove:played) visited in
+--        if (isJust result) then
+--            Just result
+--        else
+--            solve_helper (tail nextStates) played visited
 
 
--- TBRL
+--solve :: State -> [Move] -> Mem -> ((Bool, [Move]), Mem)
+--solve (State bd moves) moves_played memory =
+--    let hash = hash_board bd in
+--    case (getval hash memory) of
+--        Just result ->
+--            (result, memory)
+--        Nothing ->
+--            if win (State bd moves) then
+--                ((True, moves_played), memory)
+--            else if lose (State bd moves) then
+--                ((False, moves_played), memory)
+--            else
+--                let states_and_moves = new_states_and_moves (State bd moves) moves_played in
+--                    foldl (\ dflt new -> recurse dflt new) ((False, moves_played), memory) states_and_moves
+--
+--recurse :: ((Bool, [Move]), Mem) -> (State, [Move]) -> ((Bool, [Move]), Mem)
+--recurse dflt new =
+--    let memory = snd dflt
+--        (result, rMem) = solve (fst new) (snd new) memory
+--        hash = hash_board (get_board (fst new))
+--        newMemory = insertval hash result rMem
+--        updatedResult = if fst result
+--                            then ((True, (snd result)))
+--                        else (fst dflt)
+--        in (updatedResult, newMemory)
+--
+--
+--new_states_and_moves :: State -> [Move] -> [(State, [Move])]
+--new_states_and_moves (State bd moves) moves_played =
+--    map (\move -> ((update_game_state (State bd moves) move), (move: moves_played))) moves
 
--- RLBT
 
--- BTLR
+--search :: State -> Mem -> Int -> ((Bool, [Move]), Mem)
+--search (State board moves) memory depth
+--    | depth == 0 = ((False, []), memory)
+--    | isJust memo = (fromJust memo, memory)
+--    | win (State board moves) = ((True, []), memory)
+--    | lose (State board moves) = ((False, []), memory)
+--    | otherwise = foldl tryMove ((False, []), memory) moves
+--    where
+--        hash = hashState board
+--        memo = getval hash memory
+--        tryMove ((found, moves), newMem) move =
+--            if found then ((found, moves), newMem) else
+--            let newState = update_game_state (State board moves) move
+--                ((result, newMoves), rMem) = search newState newMem (depth - 1)
+--                finalMem = insertval hash (result, newMoves) rMem
+--            in if result then ((True, move:newMoves), finalMem) else ((found, moves),finalMem)
+--        hashState board =
+--            hash (map snd $ filter (\(coord, status) -> status /= -1) $ concat board)
+--                    where hash xs = foldl' hashStep 0 xs
+--                          hashStep a b = 31 * a + b
 
--- TBLR
+rotate_board1 board
+   | board == [] = []
+   | otherwise = let (row:rows) = board in row : rotate_board1 rows
 
--- RLTB
+rotate_board2 board
+    | board == [] = []
+    | null (head board) = []
+    | otherwise = (map head board) : rotate_board2(map tail board)
 
--- RLBT
+rotate_board3 board = reverse (rotate_board2 board)
 
--- LRBT
+rotate_board4 board
+    | board == [] = []
+    | otherwise = let (row:rows) = board in (reverse row) : rotate_board4 rows
+
+rotate_board5 board = reverse (rotate_board4 board)
+
+rotate_board6 = map reverse . transpose
+
+rotate_board7 board = transpose $ rotate_board6 board
+
+rotate_board8 board = transpose $ reverse (rotate_board4 board)
+
+hash_functions board = [rotate_board1 board, rotate_board2 board, rotate_board3 board, rotate_board4 board, rotate_board5 board, rotate_board6 board, rotate_board7 board, rotate_board8 board]
+
+--hash_functions board = [rotate_board1 board]
+
+test_hash_boards = hash_functions win_board
+
+display_hashed_boards = mapM_ display_board test_hash_boards
+
+generate_hash :: Board -> Int
+generate_hash board =
+    let collapsed = concat board
+        game_board = drop 22 (take (length collapsed - 22) collapsed)
+        arr = (map (\ (coord, status) -> if (status == -1) then 0 else status) game_board) in
+        foldl (\ num x -> num * 2 + x) 0 arr
+
+hash_board board =
+    let boards = hash_functions board
+        hashes = map generate_hash boards in
+        minimum hashes
 
 
 parse_move :: [Move] -> IO Move
